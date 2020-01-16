0592458575a3f3ecedb84fb3bc56fd468ad9c17f
Zachary Tipnis <zatipnis@icloud.com>
2020-01-16 15:34:06 -0500

    Finally got around to a leak check. Surprise! There were some...

--- Helpers.hpp
+++ Helpers.hpp
@@ -26,7 +26,7 @@ std::string gen_uuid(int len) {
   uuid_t id;
   uuid_generate((unsigned char *)&id);
   srand(1);
-  char *uuid = new char[len + 1];
+  std::string uuid(len, 0);
   uuid[len] = '\0';
   const char alphanum[36] = {'1', '2', '3', '4', '5', '6', '7', '8', '9',
                              '0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
@@ -36,8 +36,7 @@ std::string gen_uuid(int len) {
     srand(reinterpret_cast<unsigned int &>(id[i % 16]) + rand());
     uuid[i] = alphanum[rand() % 36];
   }
-  std::string uuid_r(uuid);
-  return uuid_r;
+  return uuid;
 }
 
 inline void sendMsg(int fd, const std::string &data) {
